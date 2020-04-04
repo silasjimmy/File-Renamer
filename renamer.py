8b7a9e9a481da097dcf352bcd17d9007059f7f64
polojerry <polojerry4@gmail.com>
2020-04-04 15:52:57 +0300

    replaced space " " with "_" in the name

--- renamer.py
+++ renamer.py
@@ -3,12 +3,12 @@ from os import path
 
 
 def main():
-    file_dir = "/home/polo/Pictures/rename"
+    file_dir = "/home/polo/Pictures/Delvis Photos/whiskey/m--->>"
     if path.exists(file_dir):
         files = os.listdir(file_dir)
-        png_files = [_ for _ in files if _[-4:] == ".png"]
+        png_files = [_ for _ in files if _[-5:] == ".jpeg"]
         for file in png_files:
-            print(file)
+            print(file.lower().replace(" ", "_"))
 
 
 if __name__ == '__main__':
