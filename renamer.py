e3ace124f1ca7cd56ab3647a758b4185934f84c2
polojerry <polojerry4@gmail.com>
2020-04-04 16:19:33 +0300

    Renamed the file using custom name == replaced " "(space) with _ and changes the case to lowercase

--- renamer.py
+++ renamer.py
@@ -5,10 +5,12 @@ from os import path
 def main():
     file_dir = "/home/polo/Pictures/Delvis Photos/whiskey/m--->>"
     if path.exists(file_dir):
-        files = os.listdir(file_dir)
-        png_files = [_ for _ in files if _[-5:] == ".jpeg"]
-        for file in png_files:
-            print(file.lower().replace(" ", "_"))
+        file_names = os.listdir(file_dir)
+        jpeg_files_names = [_ for _ in file_names if _[-5:] == ".jpeg"]
+        for file_name in jpeg_files_names:
+            src_name = file_dir + "/" + file_name
+            dest_name = file_dir + "/" + file_name.lower().replace(" ", "_")
+            os.rename(src_name, dest_name)
 
 
 if __name__ == '__main__':
